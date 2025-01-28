4bfe7e6a2630d5f34554bac2b2729c6652b1d8cc
Garuinja <davidsurkalim1609@gmail.com>
2025-01-28 15:35:56 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 function setup() {
   createCanvas(400, 400);
-  background(200);
-  setTimeout(makeButterfly, 2000);
-  setInterval(makePacman, 50);
+  background(220);
+  setInterval(makePacman, 500);
 }
 
 function mousePressed(){
-  setTimeout(makeButterfly);
+  setTimeout(makeButterfly, 2000);
 }
 
 // function draw() {
@@ -20,14 +19,19 @@ function mousePressed(){
 //   // triangle(120,170,130,210,170,200);
 // }
 function makeButterfly(){
-  fill(239, 195, 202);
+  noStroke();
+  fill(237, 129, 29);
   let y = random(0,height);
   let x = random(0,width);
   quad(x,y, x+20,y+50, x+60,y, x+40,y+50);
 }
 function makePacman(){
+  noStroke();
   fill(255,255,0);
   let y = random(0, height);
   let x = random(0, width);
-  arc(x,y,80,80,0,PI+HALF_PI,PIE);
+  arc(x,y,80,80,0,6,PIE);
+  fill(220);
+  arc(x, y, 80, 80, 0, 0.6, PIE);
+  stroke();
 }
